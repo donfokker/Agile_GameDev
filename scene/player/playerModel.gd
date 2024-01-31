1e6a5da8576fa4ccf50d46b252eebc14ff09d3a3
DoniBoye <donfokker@gmail.com>
2024-01-31 13:21:20 +0100

    update

--- scene/player/playerModel.gd
+++ scene/player/playerModel.gd
@@ -1,11 +1,23 @@
 extends CharacterBody3D
 
 const SPEED = 5;
-const jump_VELOCITY = 4.5;
+const jump_VELOCITY = 8;
+
+var mouse_sens = 0.05;
+@onready var head = $Head
 
 #get input from the project settings to be synced with the RigidBody nodes
 #var gravity = ProjectSettings.get_setting("physic/3d/default.gravity")
-var gravity = 9;
+var gravity = 15;
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.y * mouse_sens))
+		head.rotate_x(deg_to_rad(-event.relative.x * mouse_sens))
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-90), deg_to_rad(90))
 
 func _physics_process(delta):
 	#Add the gravity
@@ -17,13 +29,13 @@ func _physics_process(delta):
 		velocity.y = jump_VELOCITY
 	
 	#Get the input direction and handle the movement/deceleration
-	var input_dir = Input.get_vector("move_forward", "move_backward", "move_left", "move_right")
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.x = move_toward(velocity.z, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
 	
 	move_and_slide()
